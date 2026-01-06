2bddab7da43c36085eba34701fe38881026579f5
Dulaney Robotics <contact@team1727.org>
2026-01-06 15:35:44 -0500

    Testing Git

--- 2026-Robot/src/main/java/frc/robot/Constants.java
+++ 2026-Robot/src/main/java/frc/robot/Constants.java
@@ -15,9 +15,5 @@ package frc.robot;
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
-    //testing git//
-
-    public static final int hi=9;
-    
   }
 }
